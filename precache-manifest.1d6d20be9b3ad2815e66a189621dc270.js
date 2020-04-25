15f6e0a0ada23d31985f8c2793820cab57fe0383
Oscar <oscar.ramos@ucsp.edu.pe>
2020-04-25 12:51:06 -0500

    Updates

---
+++ precache-manifest.1d6d20be9b3ad2815e66a189621dc270.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a1b3c829aa0da8b58169723b3632b2",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "18af6a662940ec9fe2a2",
+    "url": "/calculator/static/css/main.6bce5863.chunk.css"
+  },
+  {
+    "revision": "29b58fca1b99c09c521a",
+    "url": "/calculator/static/js/2.e4a0cad7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.e4a0cad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18af6a662940ec9fe2a2",
+    "url": "/calculator/static/js/main.e791cdf0.chunk.js"
+  },
+  {
+    "revision": "76b2aec7ab6e360446a5",
+    "url": "/calculator/static/js/runtime-main.a1af8741.js"
+  }
+]);
