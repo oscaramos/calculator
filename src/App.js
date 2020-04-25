7f00ff878dba4128a4f293bf9526e6a3b2acbb78
Oscar <oscar.ramos@ucsp.edu.pe>
2020-04-25 00:06:35 -0500

    Web design complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <div className='calculator'>
+          <div className='screen'>
+            <div className='screen'>
+              <div className='screen-text'>123456789</div>
+            </div>
+          </div>
+          <div className='keys'>
+            <div className='operations'>
+              <div className='key numeric-key'><span>AC</span></div>
+              <div className='key numeric-key'><span>C</span></div>
+              <div className='key numeric-key'><span>?</span></div>
+            </div>
+            <div className='numeric'>
+              <div className='key numeric-key'><span>7</span></div>
+              <div className='key numeric-key'><span>8</span></div>
+              <div className='key numeric-key'><span>9</span></div>
+              <div className='key numeric-key'><span>4</span></div>
+              <div className='key numeric-key'><span>5</span></div>
+              <div className='key numeric-key'><span>6</span></div>
+              <div className='key numeric-key'><span>1</span></div>
+              <div className='key numeric-key'><span>2</span></div>
+              <div className='key numeric-key'><span>3</span></div>
+              <div className='key numeric-key'><span>+/-</span></div>
+              <div className='key numeric-key'><span>0</span></div>
+              <div className='key numeric-key'><span>.</span></div>
+            </div>
+            <div className='operators'>
+              <div className='key operator-key'><span>/</span></div>
+              <div className='key operator-key'><span>x</span></div>
+              <div className='key operator-key'><span>-</span></div>
+              <div className='key operator-key'><span>+</span></div>
+              <div className='key operator-key'><span>=</span></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
